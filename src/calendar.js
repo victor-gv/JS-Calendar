9f3a0578595149cdbf1a04b42338f3dd1708cc55
jose-cuevas <jgutifer@gmail.com>
2022-05-02 13:01:51 +0200

    prev next funtion js

--- src/calendar.js
+++ src/calendar.js
@@ -1,14 +1,56 @@
-const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-const calendarMonthDisplay = document.getElementById('calendarMonthDisplay');
-const previousMonth = document.getElementById('previousMonth');
-const nextMonth = document.getElementById('nextMonth');
-const currentYear = document.getElementById('currentYear');
-const currentMonth = document.getElementById('currentMonth');
-const calendarDays = document.getElementById('calendarDays');
+// DOM elements
+const calendarMonthDisplay = document.getElementById("calendarMonthDisplay");
+const previousMonth = document.getElementById("previousMonth");
+const nextMonth = document.getElementById("nextMonth");
+const currentYearDisplay = document.getElementById("currentYear");
+const currentMonthDisplay = document.getElementById("currentMonth");
+const calendarDays = document.getElementById("calendarDays");
 
+// Current day
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
 
+currentMonthDisplay.textContent = months[currentMonth];
+currentYearDisplay.textContent = currentYear;
 
+previousMonth.addEventListener("click", showPreviousMonth);
+nextMonth.addEventListener("click", showNextMonth);
 
-console.log(calendarMonthDisplay)
-console.log(previousMonth)
+// Functions
+
+function showPreviousMonth() {
+  if (currentMonth !== 0) {
+    currentMonth--;
+  } else {
+    currentMonth = 11;
+    currentYear--;
+  }
+}
+function showNextMonth() {
+  if (currentMonth !== 11) {
+    currentMonth++;
+  } else {
+    currentMonth = 0;
+    currentYear++;
+  }
+}
+
+console.log(calendarMonthDisplay);
+console.log(previousMonth);
