4214f38f0329dcb432cb543d60c55bc0e0fa6241
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-05-02 10:47:22 +0200

    Added basic variables for month and day display

---
+++ src/calendar.js
@@ -0,0 +1,14 @@
+const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+const calendarMonthDisplay = document.getElementById('calendarMonthDisplay');
+const previousMonth = document.getElementById('previousMonth');
+const nextMonth = document.getElementById('nextMonth');
+const currentYear = document.getElementById('currentYear');
+const currentMonth = document.getElementById('currentMonth');
+const calendarDays = document.getElementById('calendarDays');
+
+
+
+
+console.log(calendarMonthDisplay)
+console.log(previousMonth)
